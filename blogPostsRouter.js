4f6177e185b788c2f43eded3edef098d336e7ae1
moexu13 <moexu13@gmail.com>
2017-04-11 23:48:03 -0400

    Update HTTP actions with correct fields. Modularize routes

---
+++ blogPostsRouter.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+// Add a couple of blog posts on server load so you"ll automatically have some data to look at when the server starts
+BlogPosts.create("Blog 1", "This is my first blog", "author1");
+BlogPosts.create("Blog 2", "This is my second blog", "author1");
+BlogPosts.create("Blog 3", "Had pasta for lunch today", "author2");
+
+// GET and POST requests should go to /blog-posts.
+router.get("/", (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+router.post('/', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(
+    req.body.title, req.body.content, req.body.author);
+  res.status(201).json(item);
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = [
+    'id', 'title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post with id \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).json(updatedItem);
+});
+
+router.delete('/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post with id \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,76 +1,13 @@
 const express = require('express');
-const router = express.Router();
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
-const jsonParser = bodyParser.json();
+const blogPostsRouter = require('./blogPostsRouter');
 const app = express();
 
-const {BlogPosts} = require('./models');
-
-// log the http layer
 app.use(morgan('common'));
 
-// Add a couple of blog posts on server load so you'll automatically have some data to look at when the server starts
-// TODO: figure out how to initialize these
-BlogPosts.create();
-BlogPosts.create();
-BlogPosts.create();
+app.use('/blog-posts', blogPostsRouter);
 
-// TODO: Use Express router and modularize routes to /blog-posts.
-
-// GET and POST requests should go to /blog-posts.
-
-app.get('/blog-posts', (req, res) => {
-  res.json(BlogPosts.get());
-});
-
-app.post('/blog-posts', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body - TODO: fix this with proper fields
-  const requiredFields = ['name', 'budget'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-
-  const item = BlogPosts.create(req.body.name, req.body.budget); // TODO: this too
-  res.status(201).json(item);
-});
-
-// DELETE and PUT requests should go to /blog-posts/:id.
-app.put('/blog-posts/:id', jsonParser, (req, res) => {
-  const requiredFields = ['name', 'budget', 'id']; // TODO: replace with proper fields
-  for (let i=0; i<requiredFields.length; i++) {		// TODO: post date is optional; use today by default
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  if (req.params.id !== req.body.id) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id `
-      `(${req.body.id}) must match`);
-    console.error(message);
-    return res.status(400).send(message);
-  }
-  console.log(`Updating blog post item \`${req.params.id}\``);
-  // TODO: replace with proper fields
-  const updatedItem = BlogPosts.update({
-    id: req.params.id,
-    name: req.body.name,
-    budget: req.body.budget
-  });
-  res.status(204).json(updatedItem);
-});
-
-app.delete('/blog-posts/:id', (req, res) => {
-  BlogPosts.delete(req.params.id);
-  console.log(`Deleted blog post \`${req.params.ID}\``);
-  res.status(204).end();
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
